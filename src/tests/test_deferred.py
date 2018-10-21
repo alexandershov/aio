037e3aa580c84a11ec65865c6f19b4e2a120f612
aershov <codumentary.com@gmail.com>
2018-10-21 18:08:48 +0300

    test two callbacks

--- src/tests/test_deferred.py
+++ src/tests/test_deferred.py
@@ -6,3 +6,11 @@ def test_callback():
     d.add_callback(lambda result: result + 1)
     d.callback(8)
     assert d.result == 9
+
+
+def test_two_callbacks():
+    d = aio.Deferred()
+    d.add_callback(lambda result: result + 1)
+    d.add_callback(lambda result: result * 2)
+    d.callback(8)
+    assert d.result == 18
