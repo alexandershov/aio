07eee8facc326c225c2a9aed8d05dc2e2cf3a483
aershov <codumentary.com@gmail.com>
2018-10-30 21:29:22 +0300

    run loop callbacks

--- src/aio/loop.py
+++ src/aio/loop.py
@@ -1,15 +1,25 @@
+import collections
+
+
 class Loop:
+    def __init__(self):
+        self._running = False
+        self._callbacks = collections.deque()
+
     def call_soon(self, callback):
-        pass
+        self._callbacks.append(callback)
 
     def stop(self) -> None:
-        pass
+        self._running = False
 
     def is_running(self) -> bool:
-        pass
+        return self._running
 
     def run_forever(self) -> None:
-        pass
+        self._running = True
+        while self._callbacks:
+            callback = self._callbacks.popleft()
+            callback()
 
 
 def get_event_loop() -> Loop:
--- src/tests/test_loop.py
+++ src/tests/test_loop.py
@@ -1,7 +1,20 @@
 import aio
 
+import aio.loop
+
 
 def test_call_soon():
     loop = aio.get_event_loop()
-    loop.call_soon(loop.stop)
+    assert not loop.is_running()
+    loop.call_soon(_Stopper(loop))
     loop.run_forever()
+    assert not loop.is_running()
+
+
+class _Stopper:
+    def __init__(self, loop: aio.loop.Loop):
+        self._loop = loop
+
+    def __call__(self):
+        assert self._loop.is_running()
+        self._loop.stop()
