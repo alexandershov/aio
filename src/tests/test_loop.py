d9a892d6195f6e135df07fc4e412ab0c95a52f74
aershov <codumentary.com@gmail.com>
2018-10-31 09:23:27 +0300

    test call_at

--- src/tests/test_loop.py
+++ src/tests/test_loop.py
@@ -19,6 +19,15 @@ def test_call_soon(loop):
     assert not loop.is_running()
 
 
+def test_call_at(loop):
+    calls = []
+    loop.call_later(0.002, lambda: calls.append(2))
+    loop.call_later(0.001, lambda: calls.append(1))
+    loop.call_later(0.003, _Stopper(loop))
+    loop.run_forever()
+    assert calls == [1, 2]
+
+
 class _Stopper:
     def __init__(self, loop: aio.loop.Loop):
         self._loop = loop
