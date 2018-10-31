b9ddb9fe244175cc0832f561d090a3c75bd019f0
aershov <codumentary.com@gmail.com>
2018-10-31 09:18:05 +0300

    move callbacks to priority queue

--- src/aio/loop.py
+++ src/aio/loop.py
@@ -1,14 +1,20 @@
-import collections
+import heapq
 import time
+import typing as tp
+from dataclasses import dataclass, field
 
 
 class Loop:
     def __init__(self):
         self._running = False
-        self._callbacks = collections.deque()
+        self._callbacks = []
 
     def call_soon(self, callback):
-        self._callbacks.append(callback)
+        return self.call_later(0, callback)
+
+    def call_later(self, delay, callback):
+        eta = time.monotonic() + delay
+        heapq.heappush(self._callbacks, _ScheduledCallback(eta, callback))
 
     def stop(self) -> None:
         self._running = False
@@ -22,8 +28,19 @@ class Loop:
             if not self._callbacks:
                 time.sleep(1)
             else:
-                callback = self._callbacks.popleft()
-                callback()
+                callback = self._callbacks[0]
+                now = time.monotonic()
+                if callback.eta > now:
+                    time.sleep(callback.eta - now)
+                else:
+                    callback = heapq.heappop(self._callbacks)
+                    callback.callback()
+
+
+@dataclass(frozen=True, order=True)
+class _ScheduledCallback:
+    eta: float
+    callback: tp.Callable = field(compare=False)
 
 
 def get_event_loop() -> Loop:
