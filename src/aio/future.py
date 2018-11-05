4d061fcdc22d326cbc023f0f7fe115e2809c1c60
aershov <codumentary.com@gmail.com>
2018-11-05 18:05:56 +0300

    add TODO

--- src/aio/future.py
+++ src/aio/future.py
@@ -47,6 +47,7 @@ class Future:
         return self._done
 
     # TODO: should I trigger callback chain on set_exception()?
+    # TODO: what should I do if this future is already done?
     def add_done_callback(self, callback):
         self._callbacks.append(callback)
 
