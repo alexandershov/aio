f0837ca4beaa619eb7d17c182ffc280195994d33
aershov <codumentary.com@gmail.com>
2018-11-06 21:51:47 +0300

    _callbacks_count -> num_callbacks

--- src/aio/_loop.py
+++ src/aio/_loop.py
@@ -11,7 +11,7 @@ class Loop:
     def __init__(self):
         self._running = False
         self._callbacks = []
-        self._callbacks_count = 0
+        self._num_callbacks = 0
 
     def call_soon(self, callback, *args):
         return self.call_later(0, callback, *args)
@@ -21,8 +21,8 @@ class Loop:
         return self.call_at(eta, callback, *args)
 
     def call_at(self, eta, callback, *args):
-        i = self._callbacks_count
-        self._callbacks_count += 1
+        i = self._num_callbacks
+        self._num_callbacks += 1
         heapq.heappush(self._callbacks, _ScheduledCallback(eta, i, callback, args))
 
     def time(self) -> float:
