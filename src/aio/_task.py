7a89e0f2fd67381695adc3753c1a896c282c6659
aershov <codumentary.com@gmail.com>
2018-11-06 23:15:19 +0300

    use lambda instead of synthetic method

--- src/aio/_task.py
+++ src/aio/_task.py
@@ -16,8 +16,4 @@ class Task(aio.Future):
         else:
             if not isinstance(future, aio.Future):
                 raise RuntimeError(f'{future!r} is not a future')
-            future.add_done_callback(self._on_future_done)
-
-    def _on_future_done(self, future):
-        del future  # unused
-        self._step()
+            future.add_done_callback(lambda _: self._step())
