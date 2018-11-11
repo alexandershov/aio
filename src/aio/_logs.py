132df1752bd835813d0ad1e45c8caaff33e76240
aershov <codumentary.com@gmail.com>
2018-11-11 13:18:46 +0300

    Add log format

--- src/aio/_logs.py
+++ src/aio/_logs.py
@@ -4,7 +4,10 @@ import logging
 def _setup_logging():
     logger = logging.getLogger('aio')
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
 
 # TODO: maybe it should be called when event loop is running?
