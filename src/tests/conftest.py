0eeae8cdedd83075f9af598722f5aa75c4272d27
aershov <codumentary.com@gmail.com>
2018-11-11 13:19:59 +0300

    Explicitly ignore unused arguments

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -5,9 +5,11 @@ import aio
 
 @pytest.fixture(name='future')
 def future_fixture(request):
+    del request  # unused
     return aio.Future()
 
 
 @pytest.fixture(name='loop')
 def loop_fixture(request):
+    del request  # unused
     return aio.get_event_loop()
