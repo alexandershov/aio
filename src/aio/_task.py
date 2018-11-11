1fa9a320e245b56521316a86574bd5f93e828003
aershov <codumentary.com@gmail.com>
2018-11-11 11:02:00 +0300

    Add 'pending' status to Task

--- src/aio/_task.py
+++ src/aio/_task.py
@@ -9,16 +9,19 @@ class Task(aio.Future):
     def __init__(self, coro):
         super().__init__()
         self._coro = coro
+        self._status = 'pending'
         loop = aio.get_event_loop()
         loop.call_soon(self._run)
         logger.debug('Created %s', self)
 
     def _run(self):
+        self._status = 'running'
         logger.debug('Running %s', self)
         try:
             future = self._coro.send(None)
         except StopIteration as exc:
             # TODO: what about if there's some exception?
+            self._status = 'done'
             self.set_result(exc.value)
         else:
             if not isinstance(future, aio.Future):
@@ -26,9 +29,4 @@ class Task(aio.Future):
             future.add_done_callback(lambda _: self._run())
 
     def __str__(self):
-        return f'<Task {self._status()} {self._coro}>'
-
-    def _status(self):
-        if self.done():
-            return 'finished'
-        return 'running'
+        return f'<Task {self._status} {self._coro}>'
