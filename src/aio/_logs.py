ff75bb30ee6ee33989f43092e73381374086130d
aershov <codumentary.com@gmail.com>
2018-11-12 09:40:20 +0300

    Add NullHandler in _configure_logging

--- src/aio/_logs.py
+++ src/aio/_logs.py
@@ -3,8 +3,8 @@ import logging
 
 def _configure_logging():
     logger = logging.getLogger('aio')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    logger.addHandler(logging.NullHandler())
+
+
+# TODO: look at when `requests` calls it
+_configure_logging()
