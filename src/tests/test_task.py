33d16ed5558c0e39f3ac77a39e8610937279baae
aershov <codumentary.com@gmail.com>
2018-11-14 09:48:46 +0300

    Close unused coroutines in `coro` fixture

--- src/tests/test_task.py
+++ src/tests/test_task.py
@@ -6,11 +6,12 @@ import aio
 @pytest.fixture(name='coro')
 def coro_fixture(request):
     del request  # unused
-    return _coro_pass()
+    coro = _coro_pass()
+    yield coro
+    coro.close()
 
 
 def test_doesnt_support_set_result(coro):
-    # TODO: get rid of warning '_coro_pass' was never awaited
     task = aio.Task(coro)
     with pytest.raises(RuntimeError):
         task.set_result(9)
