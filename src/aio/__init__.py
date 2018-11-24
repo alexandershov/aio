8b44746e837152e363fbfe5edaff996325e70318
aershov <codumentary.com@gmail.com>
2018-11-24 11:57:25 +0300

    Add Handle & TimerHandle to aio/__init__.py

--- src/aio/__init__.py
+++ src/aio/__init__.py
@@ -3,9 +3,10 @@ from ._errors import CancelledError, InvalidStateError
 from ._future import Future
 
 from ._loop import (
-    Loop,
+    run, get_running_loop,
     get_event_loop, new_event_loop, set_event_loop,
-    run, get_running_loop)
+    Loop,
+    Handle, TimerHandle)
 
 from ._task import Task, ensure_future, current_task, all_tasks
 
