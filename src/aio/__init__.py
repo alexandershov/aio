b3223e3083686aeb48ea9e8b7f156b405380fda8
aershov <codumentary.com@gmail.com>
2018-11-24 10:03:02 +0300

    Add NullHandler in a separate function

--- src/aio/__init__.py
+++ src/aio/__init__.py
@@ -9,7 +9,10 @@ from ._loop import (
 
 from ._task import Task, ensure_future, current_task, all_tasks
 
-import logging
 
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-del logging
+def _configure_logging():
+    import logging
+    logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+
+_configure_logging()
